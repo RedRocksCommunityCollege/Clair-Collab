8d2a4b090d265f68d3c118b241a4fb83b6387b5a
CoryK8nn8dy <k8nn8dy@gmail.com>
2017-09-15 07:22:40 -0600

    changed code to NN

--- Tests/tensorflow_practice.py
+++ Tests/tensorflow_practice.py
@@ -1,18 +1,29 @@
-# Import 'tensorflow'
 import tensorflow as tf
 
-# Initialize two constants
-x1 = tf.constant([1,2,3,4])
-x2 = tf.constant([5,6,7,8])
+# Model parameters
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+# Model input and output
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+y = tf.placeholder(tf.float32)
 
-# Multiply
-result = tf.multiply(x1, x2)
+# loss
+loss = tf.reduce_sum(tf.square(linear_model - y)) # sum of the squares
+# optimizer
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
 
-# Initialize the Session
+# training data
+x_train = [1, 2, 3, 4]
+y_train = [0, -1, -2, -3]
+# training loop
+init = tf.global_variables_initializer()
 sess = tf.Session()
+sess.run(init) # reset values to wrong
+for i in range(1000):
+  sess.run(train, {x: x_train, y: y_train})
 
-# Print the result
-print(sess.run(result))
-
-# Close the session
-sess.close()
+# evaluate training accuracy
+curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
+print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
