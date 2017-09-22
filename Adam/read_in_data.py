e532d90d7e63508d29e62cea54b938b18195a454
RRCCDataLab <adam.forland@rrcc.edu>
2017-09-21 22:03:21 -0600

    Started work on data

---
+++ Adam/read_in_data.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+
+Data_Frame_Clump = pd.read_csv('/home/adam/GitHub/RedRocksCommunityCollege/Clair-Global-Collab/Data/secure-devices.csv', error_bad_lines=False)
+Data_Frame_Clump['X_type'].unique()
+traffic = Data_Frame_Clump[Data_Frame_Clump['X_type'].str.contains('traffic', na = False)]
+event = Data_Frame_Clump[Data_Frame_Clump['X_type'].str.contains('event', na = False)]
+utm = Data_Frame_Clump[Data_Frame_Clump['X_type'].str.contains('utm', na = False)]
+X_131072 = Data_Frame_Clump[Data_Frame_Clump['X_type'].str.contains('131072', na = False)]
+app_ctrl_all = Data_Frame_Clump[Data_Frame_Clump['X_type'].str.contains('app-ctrl-all', na = False)]
+X_262144 = Data_Frame_Clump[Data_Frame_Clump['X_type'].str.contains('262144', na = False)]
+
+i = 0
+sites = np.array([])
+for i in range(163):
+    sites = np.append(sites , [utm['app'].unique()[i], utm['app'].str.count(str(utm['app'].unique()[i])).sum()])
+    i = i + 1
+
+Data_Sites = {'Sites' : sites[::2], 'Visits' : sites[1::2]}
+Data_Frame_Sites = pd.DataFrame(Data_Sites)
+
+utm['app'].unique()
+
+len(utm['app'].unique())
+len(traffic['srccountry'].unique())
+
+.000000001 * pd.to_numeric(traffic['sentbyte']).sum()
+.000000001 * pd.to_numeric(traffic['rcvdbyte']).sum()
