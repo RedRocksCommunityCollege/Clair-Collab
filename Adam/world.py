e50c68ee451df982e69425ce971c42352530c9ae
RRCCDataLab <adam.forland@rrcc.edu>
2017-10-12 20:23:53 -0600

    Trying vincent

--- Adam/world.py
+++ Adam/world.py
@@ -1,7 +1,8 @@
 import vincent
 geo_data = [{'name': 'countries',
-             'url': 'https://github.com/wrobstory/vincent_map_data/blob/master/world-countries.json',
+             'url': 'https://github.com/wrobstory/vincent_map_data/blob/master/world-countries.topo.json',
              'feature': 'world-countries'}]
 
 vis = vincent.Map(geo_data=geo_data, scale=200)
-vis.to_json('vega.json')
+vis.to_json('vega.json', html_out=True, html_path='vega_template.html')
+vis.display()
