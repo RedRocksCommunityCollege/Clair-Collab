794aedc92b18d27e30ae32c5eb50a1c1cb13ce68
CoryK8nn8dy <k8nn8dy@gmail.com>
2017-11-06 12:18:11 -0700

    Created DF with bandwidth values and countries

--- Tests/Bandwidth_by_Country.py
+++ Tests/Bandwidth_by_Country.py
@@ -1,7 +1,20 @@
+import pandas as pd
+
+df_eventa = pd.read_csv('https://raw.githubusercontent.com/RedRocksCommunityCollege/Clair-Global-Collab/master/Data/secure-devices.csv', nrows = 2390 , error_bad_lines=False)
+
+df_countries_represented = pd.read_csv('https://raw.githubusercontent.com/RedRocksCommunityCollege/Clair-Global-Collab/master/Data/Countries_Represented.csv',error_bad_lines=False)
+
+code_dict = dict(zip(df_countries_represented.Country, df_countries_represented.Code))
+
+df_countries_bandwidth = pd.DataFrame({'Country': df_eventa['srccountry'], 'Code': df_eventa['srccountry'].map(code_dict), 'Sent Byte': df_eventa['sentbyte'], 'Rcvd Byte': df_eventa['rcvdbyte'], 'Total Byte': (df_eventa['sentbyte'] + df_eventa['rcvdbyte'])})
+
+
+
+df_countries_bandwidth
+
+
+'''_______________________________ experimentation_____________________________________'''
 f_Choropleth['SentBytes'] = pd.Series(df_Choropleth['Country'](0), index=df_Choropleth.index)
-
-
-
 
 
 df_EventA = df_EventA[:1999]
@@ -29,3 +42,4 @@ Total
 
 
 df_Choropleth
+'''________________________________end_experimentation________________________________'''
