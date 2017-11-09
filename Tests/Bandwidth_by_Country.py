9f2537b6bec0422f9bedfa0db1deaf41c2db8ee5
CoryK8nn8dy <k8nn8dy@gmail.com>
2017-11-08 20:48:43 -0700

    Sorted and summed data frame. Created choropleth 'Bandwidth by Country'

--- Tests/Bandwidth_by_Country.py
+++ Tests/Bandwidth_by_Country.py
@@ -6,59 +6,47 @@ df_eventa = pd.read_csv('https://raw.githubusercontent.com/RedRocksCommunityColl
 
 df_countries_represented = pd.read_csv('https://raw.githubusercontent.com/RedRocksCommunityCollege/Clair-Global-Collab/master/Data/Countries_Represented.csv',error_bad_lines=False)
 
+# Create dictionary linking country names to their country code
 code_dict = dict(zip(df_countries_represented.Country, df_countries_represented.Code))
 
+# Create DataFrame
 df_countries_bandwidth = pd.DataFrame({'Country': df_eventa['srccountry'], 'Code': df_eventa['srccountry'].map(code_dict), 'Sent Byte': df_eventa['sentbyte'], 'Rcvd Byte': df_eventa['rcvdbyte'], 'Total Byte': (df_eventa['sentbyte'] + df_eventa['rcvdbyte'])})
 
-for item in pd.unique(df_countries_bandwidth['Country']):
-    i = 0
-    Rbyte = 0
-    Sbyte = 0
-    while i < np.shape(df_countries_bandwidth)[0]:
-        if df_countries_bandwidth['Country'][i] == item:
-            Rbyte = Rbyte + int(df_countries_bandwidth['Rcvd Byte'][i])
-            Sbyte = Sbyte + int(df_countries_bandwidth['Sent Byte'][i])
-        else:
-            pass
-        i  = i + 1
+# Sum the bandwidth values for each country
+df_countries_bandwidth = df_countries_bandwidth.groupby(['Country', 'Code'], sort=False).sum()
 
+# Realign column headers after summation
+df_countries_bandwidth = df_countries_bandwidth.reset_index()
 
-country_str = str(df_countries_bandwidth['Country'].unique)
+# Map data to choropleth
+data = [ dict(
+        type = 'choropleth',
+        locations = df_countries_bandwidth['Code'],
+        z = np.log(df_countries_bandwidth['Total Byte']+1),# Use log to compress values into color range
+        text = df_countries_bandwidth['Country'],
+        colorscale = [[0,"rgb(5, 10, 172)"],[.35,"rgb(40, 60, 190)"],[0.5,"rgb(70, 100, 245)"],\
+                    [0.6,"rgb(90, 120, 245)"],[0.7,"rgb(106, 137, 247)"],[1,"rgb(220, 220, 220)"]],
+        autocolorscale = False,
+        reversescale = True,
+        marker = dict(
+            line = dict (
+                color = 'rgb(180,180,180)',
+                width = 0.5
+            ) ),
+     ) ]
 
-df_countries_bandwidth.groupby('Country')['Rcvd Byte'].sum()['United States']
+# Set layout of choropleth
+layout = dict(
+    title = 'Bandwidth by Country',
+    geo = dict(
+        showframe = False,
+        showcoastlines = True,
+        projection = dict(
+            type = 'Mercator'
+                )
+        )
+    )
 
-df_countries_bandwidth.loc[df_countries_bandwidth['Country'] == 'United States', 'Rcvd Byte'].sum()
+fig = dict( data=data, layout=layout )
 
-df_countries_bandwidth
-
-
-'''_______________________________ experimentation_____________________________________'''
-f_Choropleth['SentBytes'] = pd.Series(df_Choropleth['Country'](0), index=df_Choropleth.index)
-
-
-df_EventA = df_EventA[:1999]
-df_Choropleth = pd.DataFrame({'Country': df_Choropleth['Country'], 'Code': df_Choropleth['Code'], 'Count': df_Choropleth['Count'], 'Rec Byte': df_EventA['rcvdbyte'] ,'Sent Byte': df_EventA['sentbyte']})
-Total = np.array(['Total Rec', 'Total Sent'])
-
-for item in pd.unique(df_Choropleth['Country']):
-    i = 0
-    Rbyte = 0
-    Sbyte = 0
-    while i < np.shape(df_Choropleth)[0]:
-        if df_Choropleth['Country'][i] == item:
-            Rbyte = Rbyte + int(df_Choropleth['Rec Byte'][i])
-            Sbyte = Sbyte + int(df_Choropleth['Sent Byte'][i])
-        else:
-            pass
-        i  = i + 1
-    Total = np.append(Total,[Rbyte,Sbyte])
-
-DataTotals = ({'Country': pd.unique(df_EventA['srccountry']), 'Rec Bytes': Total[2::2] , 'Sent Bytes': Total[3::2]})
-df_Choropleth_Bandwidth = pd.DataFrame(DataTotals)
-df_Choropleth_Bandwidth
-
-Total
-
-
-df_Choropleth
-'''________________________________end_experimentation________________________________'''
+py.offline.iplot( fig, validate=False, filename='Clair-event-world-map' )
